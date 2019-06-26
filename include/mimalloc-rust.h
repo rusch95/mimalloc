28d79e9fb5651319098abb4000dc8b85ffef02a0
Robert Rusch <rusch@mit.edu>
2019-06-26 00:49:46 -0400

    Reformat the rust header

--- include/mimalloc-rust.h
+++ include/mimalloc-rust.h
@@ -3,14 +3,10 @@
 #define __MIMALLOC_RUST_H
 
 // "os.c"
-uintptr_t _mi_align_up_rs(uintptr_t sz, size_t alignment);
-
-void* mi_align_up_ptr_rs(void* p, size_t alignment);
-
-uintptr_t _mi_align_down_rs(uintptr_t sz, size_t alignment);
-
-void* mi_align_down_ptr_rs(void* p, size_t alignment);
-
+uintptr_t  _mi_align_up_rs(uintptr_t sz, size_t alignment);
+void*      mi_align_up_ptr_rs(void* p, size_t alignment);
+uintptr_t  _mi_align_down_rs(uintptr_t sz, size_t alignment);
+void*      mi_align_down_ptr_rs(void* p, size_t alignment);
 size_t     _mi_os_page_size_rs();
 
 #endif // __MIMALLOC_RUST_H
